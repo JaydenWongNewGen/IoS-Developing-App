a745caac9ba5ba3a8d46e1d45ea96bcfd0b60b3b
Siomayeater <jaydenwongkar1832@gmail.com>
2026-01-28 15:33:08 -0500

    Initial Commit

---
+++ NewGen App/NewGen_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewGen_AppApp.swift
+//  NewGen App
+//
+//  Created by Jayden Wong on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct NewGen_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
